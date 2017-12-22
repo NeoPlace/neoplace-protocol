7bc43092d5f9a21b0c3d5ae50518c03bee7a59f0
NeoPlace team <rorol@gmail.com>
2017-12-22 23:58:55 +0100

    NPT-207 process request

--- src/providers/request.service.ts
+++ src/providers/request.service.ts
@@ -2,7 +2,6 @@ import {Injectable} from "@angular/core";
 import * as CryptoJS from 'crypto-js';
 
 
-
 @Injectable()
 export class RequestService {
   private key: string = 'TODO CHANGE';
@@ -18,5 +17,33 @@ export class RequestService {
     var encrypted = CryptoJS.AES.encrypt(JSON.stringify(request), this.key);
     var encryptedmessage = 'neoplace' + encrypted.toString();
     return encryptedmessage;
+  }
+
+  processRequest(requestContent) {
+    let content = requestContent.replace('neoplace', '');
+    var decrypted = CryptoJS.AES.decrypt(content, this.key);
+    var c = decrypted.toString(CryptoJS.enc.Utf8);
+    let request = JSON.parse(c);
+
+    if(request.type) {
+      switch(request.type) {
+        case "pay": {
+          //TODO
+          break;
+        }
+        case "pay-fiat": {
+          //TODO
+          break;
+        }
+        case "pay-article": {
+          //TODO
+          break;
+        }
+        case "pay-service": {
+          //TODO
+        }
+      }
+    }
+
   }
 }
