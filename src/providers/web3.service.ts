0c6b93034dd4745697abf731ae201c141ad0ea1b
NeoPlace team <rorol@gmail.com>
2018-02-05 10:42:27 +0100

    NPT-221 Update web3

--- src/providers/web3.service.ts
+++ src/providers/web3.service.ts
@@ -20,21 +20,9 @@ export class Web3Service {
 
   initWeb3() {
     // initialize web3
-    // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-    if (typeof window.web3 !== 'undefined') {
-      console.warn(
-        'Using web3 detected from external source.'
-      );
-      // Use Mist/MetaMask's provider
-      this.web3 = new Web3(window.web3.currentProvider);
-    } else {
-      console.warn(
-        'No web3 detected.'
-      );
-      this.web3 = new Web3(
-        new Web3.providers.HttpProvider(environment.HttpProvider)
-      );
-    }
+    this.web3 = new Web3(
+      new Web3.providers.HttpProvider(environment.HttpProvider)
+    );
   }
 
 }
