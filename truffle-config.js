0d0449d304d6c9b59d2102a5f712c6cd7ca16b99
Roland <robert@neoplace.io>
2018-07-10 22:43:24 +0200

    Add missing truffle config

---
+++ truffle-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 7545,
+      network_id: '*' // Match any network id
+    },
+    rinkeby: {
+      host: "localhost",
+      port: 8545,
+      network_id: "4", // tinkeby test network
+      gas: 4700000
+    }
+  }
+}
