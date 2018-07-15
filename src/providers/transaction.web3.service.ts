6f3a3bc7370112252b9798ee4b42f2353354da30
Roland <robert@neoplace.io>
2018-07-15 22:03:20 +0200

    Update buy web3

--- src/providers/transaction.web3.service.ts
+++ src/providers/transaction.web3.service.ts
@@ -17,6 +17,67 @@ export class TransactionWeb3Service {
     this.Transaction.setProvider(this.web3Ser.web3.currentProvider);
   }
 
+getPurchases(from): Observable<any> {
+  let meta;
 
+    return Observable.create(observer => {
+      this.Transaction
+        .deployed()
+        .then(instance => {
+          meta = instance;
+          //we use call here so the call doesn't try and write, making it free
+          return meta.getPurchases.call({
+            from: from
+          });
+        })
+        .then(value => {
+          observer.next(value);
+          observer.complete();
+        })
+        .catch(e => {
+          console.log(e);
+          observer.error(e);
+        });
+    })
+  }
+
+  buyItem(from, to, itemId, typeItem, location, pictureHash, comment, price): Observable<any> {
+    let obj = {from, to, itemId, typeItem, location, pictureHash, comment, price};
+    let meta;
+
+    return Observable.create(observer => {
+      this.Transaction
+        .deployed()
+        .then(instance => {
+          meta = instance;
+          //we use call here so the call doesn't try and write, making it free
+          return meta.buyItem(
+            to.startsWith("0x") ? to : "0x" + to,
+            this.web3Ser.web3.fromAscii(itemId),
+            this.web3Ser.web3.fromAscii(typeItem),
+            this.web3Ser.web3.fromAscii(location),
+            pictureHash,
+            this.web3Ser.web3.fromAscii(comment),
+            this.web3Ser.web3.fromAscii("sold"),
+            this.web3Ser.web3.toWei(price, "ether"),
+            {
+              from: from.startsWith("0x") ? from : "0x" + from,
+              value: this.web3Ser.web3.toWei(price, "ether"),
+              gasLimit: 3000000,
+              gas: 500000,
+              gasPrice: 4000000000
+            }
+          );
+        })
+        .then(value => {
+          observer.next(value);
+          observer.complete();
+        })
+        .catch(e => {
+          console.log(e);
+          observer.error(e);
+        });
+    })
+  }
 
 }
