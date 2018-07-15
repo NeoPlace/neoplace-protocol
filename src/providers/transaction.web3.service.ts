e7242da76b859818b0bbb344b5d6aeef3bb43a3e
Roland <robert@neoplace.io>
2018-07-15 22:05:18 +0200

    sale in web3

--- src/providers/transaction.web3.service.ts
+++ src/providers/transaction.web3.service.ts
@@ -17,6 +17,30 @@ export class TransactionWeb3Service {
     this.Transaction.setProvider(this.web3Ser.web3.currentProvider);
   }
 
+  getSales(from): Observable<any> {
+    let meta;
+
+    return Observable.create(observer => {
+      this.Transaction
+        .deployed()
+        .then(instance => {
+          meta = instance;
+          //we use call here so the call doesn't try and write, making it free
+          return meta.getSales.call({
+            from: from
+          });
+        })
+        .then(value => {
+          observer.next(value);
+          observer.complete();
+        })
+        .catch(e => {
+          console.log(e);
+          observer.error(e);
+  });
+})
+}
+
 getPurchases(from): Observable<any> {
   let meta;
 
@@ -63,6 +87,36 @@ getPurchases(from): Observable<any> {
             {
               from: from.startsWith("0x") ? from : "0x" + from,
               value: this.web3Ser.web3.toWei(price, "ether"),
+              gasLimit: 3000000,
+              gas: 500000,
+              gasPrice: 4000000000
+            }
+          );
+        })
+        .then(value => {
+          observer.next(value);
+          observer.complete();
+        })
+        .catch(e => {
+          console.log(e);
+          observer.error(e);
+        });
+    })
+  }
+
+  unlockFunds(articleId, fromAddress): Observable<any> {
+    let meta;
+
+    return Observable.create(observer => {
+      this.Transaction
+        .deployed()
+        .then(instance => {
+          meta = instance;
+          //we use call here so the call doesn't try and write, making it free
+          return meta.unlockFunds(
+            articleId,
+            {
+              from: fromAddress.startsWith("0x") ? fromAddress : "0x" + fromAddress,
               gasLimit: 3000000,
               gas: 500000,
               gasPrice: 4000000000
